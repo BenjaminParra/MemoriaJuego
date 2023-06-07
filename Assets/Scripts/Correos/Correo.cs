e2a605f350f4a0d58b60422e6e331cef1f37a696
BenjaminParra <benjamin.parra.l@usach.cl>
2023-06-07 14:05:07 -0400

    Se crean los correos

---
+++ Assets/Scripts/Correos/Correo.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum TipoCorreo
+{
+    Limpio,
+    Malicioso
+}
+[Serializable]
+
+public class Correo
+{
+    public string Nombre;
+    public TipoCorreo TipoCorreo;
+
+    [Header("Remitente - Asunto")]
+
+    public string NombreRemitente;
+    public string CorreoRemitente;
+    public Sprite IconoRemitente;
+
+    public string AsuntoCorreo;
+
+    [Header("Cuerpo Correo")]
+    [TextArea] public string CuerpoCorreo;
+
+
+
+}
---
+++ Assets/Scripts/Correos/CorreoLista.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Correos")]
+public class CorreoLista : ScriptableObject
+{
+    public Correo[] Correos;
+}
