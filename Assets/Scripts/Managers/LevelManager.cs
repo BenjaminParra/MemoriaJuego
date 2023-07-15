6d1dbc3b297978166a2efe621bc3cd8ed8bf50db
BenjaminParra <benjamin.parra.l@usach.cl>
2023-07-15 14:42:17 -0400

    se agregan asset

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public enum EstadosJuego
+{
+    Juego,
+    Pausa
+}
 
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] private Personaje personaje;
     [SerializeField] private Transform puntoReaparicion;
 
+    [SerializeField] private GameObject panelPausa;
 
+    [Header("Botones")]
+    [SerializeField] private Button buttonCerrar;
+    public static EstadosJuego estado;
+
+    private bool escPulsada;
 
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    private void Awake()
+    {
+        escPulsada = false;
+        estado = EstadosJuego.Juego;
     }
 
     // Update is called once per frame
@@ -26,5 +46,49 @@ public class LevelManager : MonoBehaviour
                 personaje.RestaurarPersonaje();
             }
         }
+        if (estado == EstadosJuego.Juego) 
+        {
+            if (Input.GetKeyDown(KeyCode.Escape) && !escPulsada)
+            {
+                panelPausa.SetActive(true);
+                Time.timeScale = 0;
+                estado = EstadosJuego.Pausa;
+                escPulsada = true;
+            }
+        }
+        
+        if (!Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            escPulsada = false;
+        }
+        
+    }
+
+    public void Renaudar() 
+    {
+        if (estado == EstadosJuego.Pausa)
+        {
+            if (!escPulsada)
+            {
+                panelPausa.SetActive(false);
+                Time.timeScale = 1;
+                estado = EstadosJuego.Juego;
+                escPulsada = false;
+            }
+        }
+    }
+
+    public void VolverMenuPrincipal() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        Time.timeScale = 1;
+        estado = EstadosJuego.Juego;
+        escPulsada = false;
+    }
+
+    public void Cerrar() 
+    {
+        Debug.Log("Saliendo");
+        Application.Quit();
     }
 }
